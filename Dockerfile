f2d3980d365b67da82a39aa57388bfadd4fdc137
rustam.sharipov <rustam.sharipov@chisw.com>
2025-02-25 11:44:27 +0400

    build: create main workflows

--- Dockerfile
+++ Dockerfile
@@ -1,24 +1,14 @@
-FROM python:3.9-slim
+# Base alpine image
+FROM python:3.11-alpine3.19
 
 # Set working directory
 WORKDIR /app
 
 # Copy script and requirements
-COPY postgres_backup.py /app/postgres_backup.py
+COPY ./postgres-kubernetes-backup/postgres_backup.py /app/postgres_backup.py
 
-# Install PostgreSQL client
-RUN apt-get update && apt-get install -y postgresql-client && rm -rf /var/lib/apt/lists/*
-
-# Set environment variables (can be overridden at runtime)
-ENV DB_NAME=""
-ENV DB_USER=""
-ENV DB_PASSWORD=""
-ENV DB_HOST="localhost"
-ENV DB_PORT="5432"
-ENV BACKUP_DIR="/backups/"
-
-# Create backup directory
-RUN mkdir -p $BACKUP_DIR
+# Install dependencies
+RUN apk --no-cache add postgresql16-client
 
 # Set entrypoint
-ENTRYPOINT ["python", "/app/postgres_backup.py"]
+CMD ["python", "/app/postgres_backup.py"]
--- postgres_backup.py
+++ postgres_backup.py
@@ -2,7 +2,7 @@ import subprocess
 import datetime
 import os
 
-def manage_backup_files(backup_dir, db_name, max_files=5):
+def manage_backup_files(backup_dir, db_name, max_files):
     files = sorted(
         [f for f in os.listdir(backup_dir) if f.startswith(db_name) and f.endswith(".sql")],
         key=lambda x: os.path.getmtime(os.path.join(backup_dir, x))
@@ -14,11 +14,12 @@ def backup_postgres(backup_dir='./'):
     if not os.path.exists(backup_dir):
         os.makedirs(backup_dir)
     
-    db_name = os.getenv('DB_NAME')
-    user = os.getenv('DB_USER')
-    password = os.getenv('DB_PASSWORD')
-    host = os.getenv('DB_HOST', 'localhost')
-    port = int(os.getenv('DB_PORT', 5432))
+    db_name = os.getenv('POSTGRES_NAME')
+    user = os.getenv('POSTGRES_USER')
+    password = os.getenv('POSTGRES_PASSWORD')
+    host = os.getenv('POSTGRES_HOST')
+    port = int(os.getenv('POSTGRES_PORT', 5432))
+    max_files = os.getenv('MAX_FILES')
     
     timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
     backup_file = f"{backup_dir}{db_name}_backup_{timestamp}.sql"
@@ -26,14 +27,14 @@ def backup_postgres(backup_dir='./'):
     env = {"PGPASSWORD": password}
     command = [
         "pg_dump", "-h", host, "-p", str(port), "-U", user, "-F", "c", "-b", "-v", "-f", backup_file, db_name
-    ]
-    
+    ]    
+
     try:
         subprocess.run(command, check=True, env=env)
-        print(f"Backup successful: {backup_file}")
+        print(f"Backup successful for database {db_name}: {backup_file}")
         manage_backup_files(backup_dir, db_name)
     except subprocess.CalledProcessError as e:
         print(f"Error during backup: {e}")
 
-# Example usage
-backup_postgres(backup_dir="/path/to/backup/")
+# Run functions for both databases
+backup_postgres(backup_dir="/backup/")
