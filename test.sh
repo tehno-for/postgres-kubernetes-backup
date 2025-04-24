f12f6122f0336fe2b6b35830403841575bb729cb
rustam.sharipov <rustam.sharipov@chisw.com>
2025-04-24 12:33:58 +0400

    build: test ci

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+a=1
+b=2
+c=2
+echo "test"
+if ((a < b)) ; then
+    if ((c < 3)); then
+        echo "b is better candidate"
+    else 
+        echo "c is better candidate"
+    fi
+fi
